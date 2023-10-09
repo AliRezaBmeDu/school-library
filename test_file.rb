6a6090c86a377991e5b7bd5cff58f26f22e869ca
AliReza <ali.reza.buet@gmail.com>
2023-10-10 01:35:07 +0600

    Add test file

---
+++ test_file.rb
@@ -0,0 +1,18 @@
+require_relative 'student'
+require_relative 'teacher'
+
+student1 = Student.new(12, "Griffindor", "Harry", "yes")
+student2 = Student.new(18, "Slitherin", "Malfoy", "no")
+
+teacher1 = Teacher.new(50, "Defense", "Lupin")
+teacher2 = Teacher.new(60, "Dark Spell", "Snape")
+
+puts student1.name # should print Harry
+puts student2.age # should print 12
+# Change the second student's age
+student2.age = 13
+puts student2.age # should print 13
+
+puts student1.can_use_services? # should print true
+puts student2.can_use_services? # should print false
+puts teacher1.can_use_services? # should print true
