89dcf9c603df4e4257a6bbdd68f12c3893723a73
AliReza <ali.reza.buet@gmail.com>
2023-10-10 12:20:22 +0600

    Add student_group

--- classroom.rb
+++ classroom.rb
@@ -3,7 +3,17 @@ class Classroom
 
     def initialize(label)
         @label = label
+        @student_group = [] # this will contain the students
     end
 
+    def add_student
+        @student_group << student # This will add a student objecgt to the students
+    end
+
+    def student_list
+        @student_group.each do |student|
+            puts "id: #{student.id}, age: #{student.age}"
+        end
+    end
 
 end
