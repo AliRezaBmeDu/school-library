b6917d0fc593c00d0f53f7c1453d96d529fa9c06
AliReza <ali.reza.buet@gmail.com>
2023-10-10 12:46:47 +0600

    Add test file

--- test_file.rb
+++ test_file.rb
@@ -1,10 +1,23 @@
 require_relative 'person'
 require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
+require_relative 'classroom'
+require_relative 'student'
 
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+classroom1 =  Classroom.new('Math Class')
+classroom2 = Classroom.new('History Class')
+
+student1 = Student.new(10, 'Dawid')
+student2 = Student.new(12, 'Bairstow')
+student3 = Student.new(18, 'Root')
+student4 = Student.new(20, 'Brook')
+
+classroom1.add_student(student2)
+classroom1.add_student(student3)
+classroom2.add_student(student1)
+classroom2.add_student(student4)
+
+# puts "#{student1.classroom}"
+puts classroom2.student_list
+
+
