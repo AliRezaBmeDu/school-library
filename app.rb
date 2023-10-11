b54487f92c5a3021728a6038da7ef9d1d64bc396
AliReza <ali.reza.buet@gmail.com>
2023-10-11 12:27:55 +0600

    Fix the create person inputs

--- app.rb
+++ app.rb
@@ -5,102 +5,107 @@ require_relative 'teacher'
 require_relative 'rental'
 
 class App
+  def initialize
+    @booklist = []
+    @rentals = []
+    @people = []
+  end
 
-    def initialize
-        @booklist = []
-        @rentals = []
-        @people = []
+  def create_person_inputs
+    puts "Enter the person's age: "
+    age = gets.chomp.to_i
+    puts 'Enter the name of the person: '
+    name = gets.chomp.upcase
+    puts 'Enter the parent permission [Y/N]'
+    par_perm_input = gets.chomp.upcase
+    parent_permission = (par_perm_input == 'Y')
+    puts "parent_permission value is #{parent_permission}"
+    puts 'Is this person a Teacher or Student?'
+    puts 'S/s Student'
+    puts 'T/t Teacher'
+    type = gets.chomp.upcase
+
+    [age, name, type]
+  end
+
+  def create_person
+    age, name, type = create_person_inputs
+    case type
+    when 'S'
+      person = Student.new(age, name)
+    when 'T'
+      puts 'What is his/her specialization: '
+      specialization = gets.chomp
+      person = Teacher.new(age, specialization, name)
+    else
+      puts 'Invalid person type'
+      return
     end
 
-    def create_person
-        puts "Enter the person's age: "
-        age = gets.chomp.to_i
-        puts "Enter the name of the person: "
-        name = gets.chomp.upcase
-        puts "Enter the parent permission [Y/N]"
-        par_perm_input = gets.chomp.upcase
-        parent_permission = (par_perm_input == 'Y')
-        puts "parent_permission value is #{parent_permission}"
-        puts "Is this person a Teacher or Student?"
-        puts "S/s Student"
-        puts "T/t Teacher"
-        type = gets.chomp.upcase
+    @people << person
+    puts "person with name '#{person.name}' has been created with ID #{person.id}"
+  end
 
-        case type
-        when "S"
-            person = Student.new(age, name)
-        when "T"
-            puts "What is his/her specialization: "
-            specialization = gets.chomp
-            person = Teacher.new(age, specialization, name)
-        else
-            puts "Invalid person type"
-            return
-        end
+  def create_book
+    puts 'Enter the book title: '
+    title = gets.chomp.upcase
+    puts 'Enter the name of the author: '
+    author = gets.chomp.upcase
+    book = Book.new(title, author)
+    puts "Book '#{book.title}' by '#{book.author}' is added to the list"
+    @booklist << book
+  end
 
-        @people << person
-        puts "person with name '#{person.name}' has been created with ID #{person.id}"
+  def list_books
+    puts 'List of Books: '
+    @booklist.each do |book|
+      puts " '#{book.title}' written by '#{book.author}' "
+    end
+  end
+
+  def list_people
+    puts 'List of People:'
+    @people.each do |person|
+      puts "id: #{person.id}, name: #{person.name}, age: #{person.age}"
+    end
+  end
+
+  def create_rental
+    puts 'Enter the name of the person'
+    name = gets.chomp.upcase
+    person = @people.find { |p| p.name == name }
+
+    if person.nil?
+      puts "#{name} is not in the list"
+      return
     end
 
-    def create_book
-        puts "Enter the book title: "
-        title = gets.chomp.upcase
-        puts "Enter the name of the author: "
-        author = gets.chomp.upcase
-        book = Book.new(title, author)
-        puts "Book '#{book.title}' by '#{book.author}' is added to the list"
-        @booklist << book
+    puts 'Enter the name of the book being rented: '
+    book_name = gets.chomp.upcase
+    book = @booklist.find { |b| b.title == book_name }
+
+    if book.nil?
+      puts "#{book_name} is not in the booklist"
+      return
     end
+    date = Time.now
+    rental = Rental.new(date, book, person)
+    @rentals << rental
+    puts "#{name} rented #{book_name} at #{date}"
+  end
 
-    def list_books
-        puts 'List of Books: '
-        @booklist.each do |book|
-            puts " '#{book.title}' written by '#{book.author}' "
-        end
+  def rental_list
+    puts 'Enter the person ID: '
+    id = gets.chomp.to_i
+    person = @people.find { |p| p.id == id }
+    if person.nil?
+      puts 'The person is not in the list. Please press 2 and check the id of the people'
+      return
     end
-
-    def list_people
-        puts "List of People:"
-        @people.each do |person|
-            puts "id: #{person.id}, name: #{person.name}, age: #{person.age}"
-        end
+    puts "The books '#{person.name}' rented are: "
+    # puts "#{person.rentals}"
+    person.rentals.each do |rental|
+      puts "Book '#{rental.book.title}' written by '#{rental.book.author}' was rented on #{rental.date}"
     end
-
-    def create_rental
-        puts "Enter the name of the person"
-        name = gets.chomp.upcase
-        person = @people.find { |p| p.name == name }
-
-        if person.nil?
-            puts "#{name} is not in the list"
-            return
-        end
-
-        puts "Enter the name of the book being rented: "
-        book_name = gets.chomp.upcase
-        book = @booklist.find { |b| b.title == book_name }
-
-        if book.nil?
-            puts "#{book_name} is not in the booklist"
-            return
-        end
-        date = Time.now
-        rental = Rental.new(date, book, person)
-        @rentals << rental
-        puts "#{name} rented #{book_name} at #{date}"
-    end
-
-    def rental_list
-        puts "Enter the person ID: "
-        id = gets.chomp.to_i
-        person = @people.find { |p| p.id == id }
-        if person == nil
-            puts "The person is not in the list. Please press 2 and check the id of the people"
-            return
-        end
-        puts "The books '#{person.name}' rented are: "
-        # puts "#{person.rentals}"
-        person.rentals.each { |rental| puts "Book '#{rental.book.title}' written by '#{rental.book.author}' was rented on #{rental.date}" }
-    end
-
+  end
 end
--- main.rb
+++ main.rb
@@ -1,47 +1,50 @@
 require_relative 'app'
 
+def display_main_menu
+  puts "\nMain Menu: "
+  puts '1. List all books'
+  puts '2. List all people'
+  puts '3. Create a person'
+  puts '4. Create a book'
+  puts '5. Rent a book'
+  puts '6. List the rentals for a person'
+  puts '7. Exit'
+end
+
 def main
-    my_app = App.new
-    puts "Welcome to the 'School Library' app. Please make your choice and press Enter"
-    loop do
-        puts "\nMain Menu: "
-        puts "1. List all books"
-        puts "2. List all people"
-        puts "3. Create a person"
-        puts "4. Create a book"
-        puts "5. Rent a book"
-        puts "6. List the rentals for a person"
-        puts "7. Exit"
+  my_app = App.new
+  puts "Welcome to the 'School Library' app. Please make your choice and press Enter"
+  loop do
+    display_main_menu
 
-        choice = gets.chomp.to_i
+    choice = gets.chomp.to_i
 
-        case choice
-        when 1
-            my_app.list_books
+    case choice
+    when 1
+      my_app.list_books
 
-        when 2
-            my_app.list_people
+    when 2
+      my_app.list_people
 
-        when 3
-            my_app.create_person
+    when 3
+      my_app.create_person
 
-        when 4
-            my_app.create_book
+    when 4
+      my_app.create_book
 
-        when 5
-            my_app.create_rental
+    when 5
+      my_app.create_rental
 
-        when 6
-            my_app.rental_list
+    when 6
+      my_app.rental_list
 
-        when 7 
-            puts "Exiting... Thank you for using the app"
-            break
-        else
-            puts "Invalid choce. Please enter a valid option"
-        end
+    when 7
+      puts 'Exiting... Thank you for using the app'
+      break
+    else
+      puts 'Invalid choce. Please enter a valid option'
     end
-
+  end
 end
 
 main
