2b05f3c8a89d6634da7071af3b67f149fb51318c
AliReza <ali.reza.buet@gmail.com>
2023-10-17 00:47:23 +0600

    Fix the storing of people data

--- storage.rb
+++ storage.rb
@@ -6,28 +6,14 @@ require_relative 'book'
 module Storage
   def store_people
     data = []
-    @people.each_with_index do |person, index|
-      person_json = case person
-        when Teacher
-          {
-            label: 'Teacher', 
-            id: person.id, 
-            age: person.age, 
-            name: person.name,
-            specialization: person.specialization
-          }
-        when Student
-          {
-            label: 'Student', 
-            id: person.id, 
-            age: person.age, 
-            name: person.name
-          }
-        else
-          puts "Unknown person type"
-          {}
-      end
-
+    @people.each_with_index do |person, _index|
+      person_json = {
+        id: person.id,
+        age: person.age,
+        name: person.name,
+        label: person.label
+      }
+      person_json['specialization'] = person.specialization if person.label == 'Teacher'
       data << person_json
     end
     open('./datastorage/people.json', 'w') { |f| f.write JSON.generate(data) }
